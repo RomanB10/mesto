fcb746bfe7e08cc59b2e445ace495ca1300e0299
Roman B <onwork.r@yandex.ru>
2022-07-15 19:18:20 +0300

    fix: удаление лишних свойств

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,29 @@
-// Находим элементы-кнопки Edit и Close
-let editButton = document.querySelector(".profile__edit-btn");
-let closeButton = document.querySelector(".popup__close-btn");
+let editButton = document.querySelector(".profile__edit-btn");//Объявляем перемнную кнопки редактирования
+let closeButton = document.querySelector(".popup__close-btn"); //Объявляем перемнную кнопки закрытия
+let popup = document.querySelector(".popup"); //Объявляем перемнную контейнера с формой
+//Объявляем перемнные с полями ввода
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+//Объявляем перемнные с текстовым содержимым профайла
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
 
 function editForm() {
-  // Обработчик «открытия/редактирования» формы,
-  let popup = document.querySelector(".popup");
+// Обработчик «открытия/редактирования» формы
   popup.classList.add("popup_opened");
 }
+// Обработчик «закрытия» формы
 function closeForm() {
-  // Обработчик «закрытия» формы,
-  let popup = document.querySelector(".popup");
-  popup.classList.toggle("popup_opened");
+  popup.classList.remove("popup_opened");
 }
-editButton.addEventListener("click", editForm);
-closeButton.addEventListener("click", closeForm);
-
-// Находим форму в DOM
-let formElement = document.querySelector(".popup");
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__name-input");
-let jobInput = document.querySelector(".popup__job-input");
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  let profileTitle = document.querySelector(".profile__title");
-  let profileSubtitle = document.querySelector(".profile__subtitle");
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
-
-  let popup = document.querySelector(".popup");
-  popup.classList.remove("popup_opened");
+  profileTitle.textContent = nameInput.value;//присваеваем значения полей ввода
+  profileSubtitle.textContent = jobInput.value;//присваеваем значения полей ввода
+  closeForm() //закрытие формы
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", editForm);// Прикрепляем обработчик к кнопке редактирования
+closeButton.addEventListener("click", closeForm);// Прикрепляем обработчик к кнопке закрытия
+popup.addEventListener("submit", formSubmitHandler);// Прикрепляем обработчик к форме
